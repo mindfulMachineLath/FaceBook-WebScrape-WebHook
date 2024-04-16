2e3efaa71604d8947f2a8247121f372da4d7fd94
undersky8027 <william.dev.service@gmail.com>
2024-04-16 03:30:21 -0500

    reset the Json architecture and fix some bugs

--- action/contentScript.js
+++ action/contentScript.js
@@ -7,23 +7,16 @@ function scrapeUserDatasFromPage() {
         const questions = Array.from(element.querySelectorAll('span.x193iq5w.xeuugli.x13faqbe.x1vvkbs.xlh3980.xvmahel.x1n0sxbx.x1lliihq.x1s928wv.xhkezso.x1gmr53x.x1cpjm7i.x1fgarty.x1943h6x.x4zkp8e.x3x7a5m.x6prxxf.xvq8zen.x1s688f.x12scifz'));
         // Collect all answer spans
         const answers = Array.from(element.querySelectorAll('span')).filter(span => span.className === 'x193iq5w xeuugli x13faqbe x1vvkbs xlh3980 xvmahel x1n0sxbx x1lliihq x1s928wv xhkezso x1gmr53x x1cpjm7i x1fgarty x1943h6x x4zkp8e x3x7a5m x6prxxf xvq8zen xo1l8bm xzsf02u');
-
-        return questions.map((question, index) => {
-            // Find a corresponding answer or return null if not found
+        const dataObject = {};
+        questions.forEach((question, index) => {
             const answer = answers[index] ? answers[index].innerHTML.trim() : null;
-            return {
-                [`question${index + 1}`]: question.innerHTML.trim(),
-                [`answer${index + 1}`]: answer
-            };
+            dataObject[`question${index + 1}`] = question.innerHTML.trim();
+            dataObject[`answer${index + 1}`] = answer;
         });
+        return dataObject;
     }
     // Map each element's innerHTML to an object with a "data" key
-    const data = Array.from(elements).map(element => {
-        const QA = getInnerData(element);
-        return {
-            "nestedData": QA
-        };
-    });
+    const data = Array.from(elements).map(element => getInnerData(element));
     console.log(selector);
     console.log(data);
     return data;
