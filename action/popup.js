6121c5b77e2ce9903ddb1d7d979e86ff0857f686
undersky8027 <william.dev.service@gmail.com>
2024-04-17 04:17:05 -0500

    add loading bar and fix some bugs

--- action/popup.js
+++ action/popup.js
@@ -5,16 +5,29 @@ scrapeUserDetails.addEventListener("click", () => {
     // Get the current active tab
     chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
         // Ensure there is at least one tab
-        if (tabs.length === 0) return;
+        if (tabs.length === 0) {
+            return;
+        }
+
+        // Show the loading indicator
+        showLoading(true);
+        //delay action
+        setTimeout(() => {
+            console.log("calculated");
+        }, 1000);
+
         // Send a message to the content script
         chrome.tabs.sendMessage(tabs[0].id, {action: "scrapeData"}, (response) => {
             if (chrome.runtime.lastError || !response) {
                 // Handle error or no response
+                showLoading(false);
                 //console.error('Error or no response:', chrome.runtime.lastError?.message);
                 return;
             }
             // Now send this data to the background script
             chrome.runtime.sendMessage({data: response.data}, (response) => {
+                // Hide the loading indicator irrespective of the response
+                showLoading(false);
                 if (response && response.status === 'success') {
                     updateStatus('Success', 'success');
                 } else if(response.status === 'failed') {
@@ -31,4 +44,9 @@ function updateStatus (message, style) {
     const statusDiv = document.getElementById('status');
     statusDiv.textContent = message;
     statusDiv.className = style;
+}
+
+function showLoading(isVisible) {
+    const loadingDiv = document.getElementById('loading');
+    loadingDiv.style.display = isVisible ? 'block' : 'none';
 }
