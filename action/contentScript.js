2471c60e83085b164ddfce4d8b872d136096c632
undersky8027 <william.dev.service@gmail.com>
2024-04-25 14:18:17 -0500

    fix the bug so that it can get the group name from the URL

--- action/contentScript.js
+++ action/contentScript.js
@@ -1,8 +1,8 @@
 function scrapeUserDatasFromPage() {
-    // Select the first <a> tag with the specified class name
-    const groupNameElement = document.querySelector('a.x1i10hfl.xjbqb8w.x1ejq31n.xd10rxx.x1sy0etr.x17r0tee.x972fbf.xcfux6l.x1qhh985.xm0m39n.x9f619.x1ypdohk.xt0psk2.xe8uvvx.xdj266r.x11i5rnm.xat24cr.x1mh8g0r.xexx8yu.x4uap5.x18d9i69.xkhd6sd.x16tdsg8.x1hl2dhg.xggy1nq.x1a2a7pz.x1heor9g.xt0b8zv.x1s688f');
-    const groupname = groupNameElement ? groupNameElement.textContent.trim() : null;
-
+    // Get the current URL and split by slashes
+    const urlSegments = window.location.href.split('/');
+    // Get the segment after three slashes (fourth segment)
+    const groupname = urlSegments.length > 4 ? urlSegments[4] : null;
     const classNames = 'x1y1aw1k x4uap5 xwib8y2 xkhd6sd';
     const selector = classNames.split(' ').map(cls => `div.${cls}`).join('');
     elements = document.querySelectorAll('div.x1y1aw1k.x4uap5.xwib8y2.xkhd6sd');
@@ -16,7 +16,7 @@ function scrapeUserDatasFromPage() {
         const questions = Array.from(element.querySelectorAll('span.x193iq5w.xeuugli.x13faqbe.x1vvkbs.xlh3980.xvmahel.x1n0sxbx.x1lliihq.x1s928wv.xhkezso.x1gmr53x.x1cpjm7i.x1fgarty.x1943h6x.x4zkp8e.x3x7a5m.x6prxxf.xvq8zen.x1s688f.x12scifz'));
         // Collect all answer spans
         const answers = Array.from(element.querySelectorAll('span')).filter(span => span.className === 'x193iq5w xeuugli x13faqbe x1vvkbs xlh3980 xvmahel x1n0sxbx x1lliihq x1s928wv xhkezso x1gmr53x x1cpjm7i x1fgarty x1943h6x x4zkp8e x3x7a5m x6prxxf xvq8zen xo1l8bm xzsf02u');
-        let dataObject = { groupname, username};
+        let dataObject = { groupname, username };
         questions.forEach((question, index) => {
             const answer = answers[index] ? answers[index].innerHTML.trim() : null;
             dataObject[`question${index + 1}`] = question.innerHTML.trim();
@@ -26,7 +26,7 @@ function scrapeUserDatasFromPage() {
     }
     // Map each element's innerHTML to an object with a "data" key
     const data = Array.from(elements).map(element => getInnerData(element));
-    
+
     // Prepend the group name to the data array
     // data.unshift({ groupName });
 
